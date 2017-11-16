36aa3e41aa9f2e015944db85342c465418074902
caijianhua <iscaijianhua@163.com>
2017-11-16 10:33:58 +0800

    修改helloword

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("啦啦啦啦!");
+        System.out.println("啊啊啊啊啊!");
+        System.out.println("eeeeeeeeee");
     }
 }
