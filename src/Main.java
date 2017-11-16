3da8c785bf3dd9f71be72bd2c733f6127cbfeb98
caijianhua <iscaijianhua@163.com>
2017-11-16 10:54:37 +0800

    小袁修改部分代码

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
-
+//小袁
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
 }
